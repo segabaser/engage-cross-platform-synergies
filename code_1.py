b2f76a0f0b73f7a9732d88a2d46eabceecca648d
segabaser <139810447+segabaser@users.noreply.github.com>
2024-04-20 15:11:50 +0200

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,13 @@
+        for (int i = 0; i < 10; i++) {
+    }
+            data[i] = (int) (Math.random() * 100) + 1;
+}
+
+
+        for (int item : data) {
+            System.out.println("Random Number: " + item);
+    public static void main(String[] args) {
+        }
+public class RandomDataGenerator {
+        int[] data = new int[10];
+        }
